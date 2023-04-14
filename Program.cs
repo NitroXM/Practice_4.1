562e43e7c0f9a745c6603034c1464652463bd3d6
NitroXM <104325512+NitroXM@users.noreply.github.com>
2023-04-14 23:56:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm sorry that you see this");
+        }
+    }
+}
